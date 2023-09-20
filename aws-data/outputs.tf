47e465034f10bb4ad59f9c87290a358678d8922e
Lê Anh Dũng <43999277+ladung@users.noreply.github.com>
2023-09-20 10:34:16 +0700

    Update outputs.tf

--- aws-data/outputs.tf
+++ aws-data/outputs.tf
@@ -17,3 +17,13 @@ output "aws_ami_ids" {
   description = "A list of AMI IDs"
   value       = { for k, v in data.aws_ami.this : k => tomap({ id = v.image_id, root_device_name = v.root_device_name }) }
 }
+
+output "ubuntu_1804_aws_ami_id" {
+  description = "AMI ID of Ubuntu 18.04"
+  value       = data.aws_ami.ubuntu_1804.id
+}
+
+output "ubuntu_2004_aws_ami_id" {
+  description = "AMI ID of Ubuntu 20.04"
+  value       = data.aws_ami.ubuntu_2004.id
+}
